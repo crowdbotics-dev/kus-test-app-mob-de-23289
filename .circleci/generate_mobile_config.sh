74006651ddeef52c983f7984d408dffede09aa86
crowdbotics-dev <engineering@crowdbotics.com>
2022-01-06 14:16:59 -0600

    Testing changes using github api

--- .circleci/generate_mobile_config.sh
+++ .circleci/generate_mobile_config.sh
@@ -136,7 +136,7 @@ jobs:
 
   ios:
     macos:
-      xcode: "13.0.0"
+      "11.1.0"
     working_directory: ~/build
 
     # use a --login shell so our "set Ruby version" command gets picked up for later steps
